e14fdd367a33008b9f7506cbd659982193c2bec0
rafa <stampeedo2000@gmail.com>
2022-07-19 23:09:54 -0300

    create test with junit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void calculateDaysBetween() {
+    }
+
+    @Test
+    void sameYearAndDifferentMonth() {
+    }
+
+    @Test
+    void allDifferent() {
+    }
+}
